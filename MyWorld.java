b331f07e39b050404bdd1d5143ec8a4b4dd08d55
Jennifer Zheng <zjy258085@gmail.com>
2025-05-16 14:18:47 -0400

    add background

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        GreenfootImage bg = new GreenfootImage("images/background.jpg");
+        bg.scale(getWidth(), getHeight());
+        setBackground(bg);
     }
 }
