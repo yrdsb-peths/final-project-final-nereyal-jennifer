f80fac806bf4f2ec901dac0d054a7f8f2383f2a1
brave29 <147207172+brave29@users.noreply.github.com>
2025-05-16 14:35:47 -0400

    fixed person jumping sequence

--- MyWorld.java
+++ MyWorld.java
@@ -3,13 +3,10 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-<<<<<<< HEAD
         GreenfootImage bg = new GreenfootImage("images/background.jpg");
         bg.scale(getWidth(), getHeight());
         setBackground(bg);
-=======
         Person person = new Person();
-        addObject(person, 150, 250);
->>>>>>> 0963e10865510179ffaad7956475c65e7db70afa
+        addObject(person, 150, 300);
     }
 }
