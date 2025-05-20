cd7613406826c86de3f037dff8bd86a99da26b52
Jennifer Zheng <zjy258085@gmail.com>
2025-05-20 13:42:17 -0400

    add haystack

--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,6 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-<<<<<<< HEAD
 
         GreenfootImage bg = new GreenfootImage("images/background.jpg");
         bg.scale(getWidth(), getHeight());
@@ -13,16 +12,8 @@ public class MyWorld extends World {
         addObject(person, 150, 250);
         Haystack haystack = new Haystack();
         addObject(haystack, 500, 267);
-
-=======
-        GreenfootImage bg = new GreenfootImage("images/background.jpg");
-        bg.scale(getWidth(), getHeight());
-        setBackground(bg);
-        Person person = new Person();
-        addObject(person, 150, 300);
-        
         Reward reward = new Reward();
         addObject(reward, 500, 200);
->>>>>>> 1256018e988aaa429c0dacad4f0de9c6fe959d06
+
     }
 }
