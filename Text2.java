f2d6fc9ab9fd17538139d3850ce3ce4434b38a47
Jennifer Zheng <zjy258085@gmail.com>
2025-05-22 14:42:18 -0400

    add title page

---
+++ Text2.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Text2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Text2 extends Actor
+{
+    /**
+     * Act - do whatever the Text2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Text2()
+    {
+        String text = "Project name";
+        int fontSize = 60;
+        Color textColor = Color.WHITE;
+        Font font = new Font("Lucida Handwriting", fontSize);
+        GreenfootImage image = new GreenfootImage(text, fontSize, textColor, null);
+        image.setFont(font);
+        setImage(image);
+        
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ TitleScreen.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Font;
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        setBackground("images/title screen.jpg");
+        addObject(new Text(), 300, 300);
+        addObject(new Text2(), 300, 200);
+        
+    }
+    
+     public void act()
+    {
+        if (Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+    
+    
+}
