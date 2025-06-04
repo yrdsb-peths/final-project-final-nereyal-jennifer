abab6801eb82b843553bacabf5a900d10e0af289
brave29 <147207172+brave29@users.noreply.github.com>
2025-06-04 14:24:16 -0400

    cemetery image

--- GameOver.java
+++ GameOver.java
@@ -17,7 +17,7 @@ public class GameOver extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 400, 1); 
-        GreenfootImage bg = new GreenfootImage("cemetery.jpg");
+        GreenfootImage bg = new GreenfootImage("cemetery1.jpg");
         bg.scale(getWidth(), getHeight());
         setBackground(bg);
         Person person = new Person();
