bb210cd88515114a889d6caa2a0b9cb7de769f6e
Jennifer Zheng <zjy258085@gmail.com>
2025-06-05 23:49:13 -0400

    edit dialogue box

--- OldLady.java
+++ OldLady.java
@@ -9,6 +9,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class OldLady extends Actor
 {
     private String[] dialogues = {"ajergb", "kayrgfbauek"};
+    private int currentDialogueIndex = -1;
+    private boolean isNearPerson = false;
+    private boolean enterPressed = false;
     private int dialogueIndex = 0;
     private DialogueBox dialogueBox;
     
@@ -18,26 +21,71 @@ public class OldLady extends Actor
         getImage().scale(40, 80);
     }
 
+    /**
     public void addedToWorld(World world) {
         dialogueBox = new DialogueBox(dialogues[dialogueIndex]);
         world.addObject(dialogueBox, getX() + 100, getY() - 60);
     }
+    */
 
-    public void act()
-    {
-        if (Greenfoot.isKeyDown("enter"))
-        {
-            updateDialogue();
-            Greenfoot.delay(10);
+    public void act() {
+        checkProximity();
+        handleDialogueInput();
+        updateDialogueBoxPosition();
+    }
+
+    private void checkProximity() {
+        Person person = (Person) getWorld().getObjects(Person.class).get(0);
+        if (person != null && !isNearPerson) {
+            int distance = (int)Math.hypot(getX() - person.getX(), getY() - person.getY());
+            if (distance < 120) {
+                isNearPerson = true;
+                currentDialogueIndex = 0;
+                showDialogue();
+            }
         }
     }
 
-    private void updateDialogue()
-    {
-        dialogueIndex = (dialogueIndex + 1) % dialogues.length;
-        if (dialogueBox != null)
-        {
-            dialogueBox.setText(dialogues[dialogueIndex]);
+    private void handleDialogueInput() {
+        if (isNearPerson && Greenfoot.isKeyDown("enter")) {
+            if (!enterPressed) {
+                currentDialogueIndex++;
+                if (currentDialogueIndex < dialogues.length) {
+                    showDialogue();
+                } 
+                else {
+                    removeDialogue();
+                    isNearPerson = false;
+                    currentDialogueIndex = -1;
+                }
+                enterPressed = true;
+            }
+        } 
+        else if (!Greenfoot.isKeyDown("enter")) {
+            enterPressed = false;
+        }
+    }
+
+    private void showDialogue() {
+        if (dialogueBox == null) {
+            dialogueBox = new DialogueBox(dialogues[currentDialogueIndex]);
+            getWorld().addObject(dialogueBox, getX() + 100, getY() - 60);
+        } 
+        else {
+            dialogueBox.setText(dialogues[currentDialogueIndex]);
+        }
+    }      
+
+    private void removeDialogue() {
+        if (dialogueBox != null) {
+            getWorld().removeObject(dialogueBox);
+            dialogueBox = null;
+        }
+    }
+
+    private void updateDialogueBoxPosition() {
+        if (dialogueBox != null) {
+            dialogueBox.setLocation(getX() + 100, getY() - 60);
         }
     }
 }
